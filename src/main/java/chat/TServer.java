43e088a6a1c7cbcbf9689a29ee381efd5cc1062e
adsinc <sinc86@gmail.com>
2016-07-07 20:03:04 +0300

    tbd

---
+++ src/main/java/chat/TServer.java
@@ -0,0 +1,82 @@
+package chat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
+
+/**
+ *
+ */
+public class TServer {
+    public static void main(String[] args) throws IOException {
+        int port = 7777;
+        SocketAddress address = new InetSocketAddress(port);
+        AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open()
+                .bind(address);
+        Attachment attachment = new Attachment();
+        attachment.server = server;
+        server.accept(attachment, new CompletionHandler<AsynchronousSocketChannel, Attachment>() {
+            @Override
+            public void completed(AsynchronousSocketChannel client, Attachment attachment) {
+                attachment.server.accept(attachment, this);
+                Attachment newAttachment = new Attachment();
+                newAttachment.client = client;
+                newAttachment.buffer = ByteBuffer.allocate(512);
+                newAttachment.readSb = new StringBuilder();
+                client.read(newAttachment.buffer, newAttachment, new CompletionHandler<Integer, Attachment>() {
+                    @Override
+                    public void completed(Integer result, Attachment attachment) {
+                        if (result != -1) {
+                            ByteBuffer buffer = attachment.buffer;
+                            buffer.flip();
+                            int limit = buffer.limit();
+                            byte[] data = new byte[limit];
+                            buffer.get(data, 0, limit);
+                            attachment.readSb.append(new String(data, Charset.forName("UTF-8")));
+                            attachment.buffer.rewind();
+                            System.out.println(attachment.readSb);
+                            attachment.client.read(attachment.buffer, attachment, this);
+                        } else {
+                            System.out.println("Received: " + attachment.readSb);
+                            try {
+                                attachment.client.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void failed(Throwable exc, Attachment attachment) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void failed(Throwable exc, Attachment attachment) {
+
+            }
+        });
+
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            //todo
+            e.printStackTrace();
+        }
+    }
+
+    private static class Attachment {
+        AsynchronousServerSocketChannel server;
+        AsynchronousSocketChannel client;
+        ByteBuffer buffer;
+        StringBuilder readSb;
+    }
+}
+
--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,10 +1,29 @@
 package client;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousChannel;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.concurrent.ExecutionException;
+
 /**
  * todo
  */
 public class Client {
     public static void main(String[] args) {
-
+        SocketAddress address = new InetSocketAddress("localhost", 7777);
+        try {
+            AsynchronousSocketChannel channel = AsynchronousSocketChannel.open();
+            channel.connect(address).get();
+            ByteBuffer buffer = ByteBuffer.wrap("Hello".getBytes());
+            channel.write(buffer).get();
+            Thread.sleep(5000);
+            channel.write(ByteBuffer.wrap("111".getBytes())).get();
+        } catch (IOException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }

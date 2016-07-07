a3d87551c5a07f55d6d4c19313b00febd34b345f
Aleksei Dolgii <sinc86@gmail.com>
2016-07-07 22:49:59 +0300

    refactoring

--- src/main/java/chat/ChatServer.java
+++ src/main/java/chat/ChatServer.java
@@ -22,66 +22,68 @@ public class ChatServer {
 
     public void start() {
         System.out.println("Server started");
-    }
-
-    private void foo(String[] args) throws IOException {
         SocketAddress address = new InetSocketAddress(port);
-        AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open()
-                .bind(address);
-        Attachment attachment = new Attachment();
-        attachment.server = server;
-        server.accept(attachment, new CompletionHandler<AsynchronousSocketChannel, Attachment>() {
-            @Override
-            public void completed(AsynchronousSocketChannel client, Attachment attachment) {
-                attachment.server.accept(attachment, this);
-                Attachment newAttachment = new Attachment();
-                newAttachment.client = client;
-                newAttachment.buffer = ByteBuffer.allocate(512);
-                newAttachment.readSb = new StringBuilder();
-                client.read(newAttachment.buffer, newAttachment, new CompletionHandler<Integer, Attachment>() {
-                    @Override
-                    public void completed(Integer result, Attachment attachment) {
-                        if (result != -1) {
-                            ByteBuffer buffer = attachment.buffer;
-                            buffer.flip();
-                            int limit = buffer.limit();
-                            byte[] data = new byte[limit];
-                            buffer.get(data, 0, limit);
-                            attachment.readSb.append(new String(data, Charset.forName("UTF-8")));
-                            attachment.buffer.rewind();
-                            attachment.client.read(attachment.buffer, attachment, this);
-                        } else {
-                            System.out.println("Received: " + attachment.readSb);
-                            try {
-                                attachment.client.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void failed(Throwable exc, Attachment attachment) {
-
-                    }
-                });
-            }
-
-            @Override
-            public void failed(Throwable exc, Attachment attachment) {
-
-            }
-        });
-
-        try {
+        try (AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open()) {
+            server.bind(address);
+            Attachment attachment = new Attachment();
+            attachment.server = server;
+            server.accept(attachment, new ConnectionHandler());
             Thread.currentThread().join();
+        } catch (IOException e) {
+            //todo
+            e.printStackTrace();
         } catch (InterruptedException e) {
             //todo
             e.printStackTrace();
         }
     }
 
-    private static class Attachment {
+    class ConnectionHandler implements CompletionHandler<AsynchronousSocketChannel, Attachment> {
+        @Override
+        public void completed(AsynchronousSocketChannel client, Attachment attachment) {
+            attachment.server.accept(attachment, this);
+            Attachment newAttachment = new Attachment();
+            newAttachment.client = client;
+            newAttachment.buffer = ByteBuffer.allocate(512);
+            newAttachment.readSb = new StringBuilder();
+            client.read(newAttachment.buffer, newAttachment, new ReadHandler());
+        }
+
+        @Override
+        public void failed(Throwable exc, Attachment attachment) {
+            //todo
+        }
+    }
+
+    class ReadHandler implements CompletionHandler<Integer, Attachment> {
+        @Override
+        public void completed(Integer result, Attachment attachment) {
+            if (result != -1) {
+                ByteBuffer buffer = attachment.buffer;
+                buffer.flip();
+                int limit = buffer.limit();
+                byte[] data = new byte[limit];
+                buffer.get(data, 0, limit);
+                attachment.readSb.append(new String(data, Charset.forName("UTF-8")));
+                attachment.buffer.rewind();
+                attachment.client.read(attachment.buffer, attachment, this);
+            } else {
+                System.out.println("Received: " + attachment.readSb);
+                try {
+                    attachment.client.close();
+                } catch (IOException e) {
+                    // todo
+                }
+            }
+        }
+
+        @Override
+        public void failed(Throwable exc, Attachment attachment) {
+            //todo
+        }
+    }
+
+    private class Attachment {
         AsynchronousServerSocketChannel server;
         AsynchronousSocketChannel client;
         ByteBuffer buffer;

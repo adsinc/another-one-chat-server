32bf4a0e84d6df220d44341eb78bb3f4a91f060d
Aleksei Dolgii <sinc86@gmail.com>
2016-07-13 00:36:50 +0300

    test

--- src/main/java/chat/client/ChatClient.java
+++ src/main/java/chat/client/ChatClient.java
@@ -135,7 +135,6 @@ public class ChatClient {
     }
 
     private void write(SelectionKey key) throws IOException {
-        System.out.println("WTINTING..........");
         SocketChannel channel = (SocketChannel) key.channel();
         channel.write(ByteBuffer.wrap(message));
         message = null;

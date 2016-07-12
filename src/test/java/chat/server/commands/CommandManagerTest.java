19912e52ce9ba358da323fe5a4129a030dbcacd2
Aleksei Dolgii <sinc86@gmail.com>
2016-07-13 01:34:02 +0300

    tests

---
+++ src/test/java/chat/server/commands/CommandManagerTest.java
@@ -0,0 +1,27 @@
+package chat.server.commands;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = "classpath:server-context.xml")
+public class CommandManagerTest {
+
+    @Test
+    public void testParseCommand() throws Exception {
+        //todo
+    }
+
+    @Test
+    public void testValidate() throws Exception {
+        //todo
+    }
+
+    @Test
+    public void testGetCommandAction() throws Exception {
+        //todo
+    }
+
+}

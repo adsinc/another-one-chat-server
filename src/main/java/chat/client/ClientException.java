790f5d36196a41925cad761441b6e22572286b08
Aleksei Dolgii <sinc86@gmail.com>
2016-07-13 00:59:52 +0300

    javadoc

--- src/main/java/chat/client/ClientException.java
+++ src/main/java/chat/client/ClientException.java
@@ -1,5 +1,8 @@
 package chat.client;
 
+/**
+ * Represents chat client application specific exceptions.
+ */
 public class ClientException extends Exception {
     public ClientException(String msg) {
         super(msg);

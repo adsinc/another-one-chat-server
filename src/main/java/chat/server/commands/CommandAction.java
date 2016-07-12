af132fdbe4620ae65fb07d2203f4468a9660dd35
Aleksei Dolgii <sinc86@gmail.com>
2016-07-13 01:16:20 +0300

    javadoc

--- src/main/java/chat/server/commands/CommandAction.java
+++ src/main/java/chat/server/commands/CommandAction.java
@@ -8,9 +8,17 @@ import java.util.Map;
 import java.util.function.BiFunction;
 
 /**
- * todo
+ * Interface for actions executed on command receive
  */
 public interface CommandAction {
+    /**
+     * Execute action for appropriate command
+     *
+     * @param commandData   CommandData object received from client
+     * @param client        client's channel
+     * @param loginToClient map of logins and appropriate channels
+     * @param replyCallBack callback used to write ServerReply to appropriate channel
+     */
     void execute(CommandData commandData,
                  SocketChannel client, Map<String, SocketChannel> loginToClient,
                  BiFunction<SocketChannel, ServerReply, Void> replyCallBack);

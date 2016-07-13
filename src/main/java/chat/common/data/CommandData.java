be245ba76cff0a51ea8593e9a9ae3e50f0a9ae1b
Aleksey Dolgiy <AVDolgiy.SBT@sberbank.ru>
2016-07-13 14:31:37 +0300

    tests

--- src/main/java/chat/common/data/CommandData.java
+++ src/main/java/chat/common/data/CommandData.java
@@ -1,5 +1,7 @@
 package chat.common.data;
 
+import java.util.Objects;
+
 /**
  * Client command data.
  */
@@ -8,4 +10,40 @@ public class CommandData {
     public String sender;
     public String message;
     public String receiver;
+
+    public CommandData() {
+    }
+
+    public CommandData(String commandName, String sender, String message, String receiver) {
+        this.commandName = commandName;
+        this.sender = sender;
+        this.message = message;
+        this.receiver = receiver;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommandData that = (CommandData) o;
+        return Objects.equals(commandName, that.commandName) &&
+                Objects.equals(sender, that.sender) &&
+                Objects.equals(message, that.message) &&
+                Objects.equals(receiver, that.receiver);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(commandName, sender, message, receiver);
+    }
+
+    @Override
+    public String toString() {
+        return "CommandData{" +
+                "commandName='" + commandName + '\'' +
+                ", sender='" + sender + '\'' +
+                ", message='" + message + '\'' +
+                ", receiver='" + receiver + '\'' +
+                '}';
+    }
 }
--- src/test/java/chat/server/commands/CommandManagerTest.java
+++ src/test/java/chat/server/commands/CommandManagerTest.java
@@ -1,10 +1,17 @@
 package chat.server.commands;
 
+import chat.common.data.CommandData;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = "classpath:server-context.xml")
@@ -13,19 +20,29 @@ public class CommandManagerTest {
     @Autowired
     private CommandManager commandManager;
 
+    private final Map<String, CommandData> testCommands = new HashMap<>();
+
+    {
+        testCommands.put("{}", new CommandData());
+        testCommands.put("{\"commandName\": \"c\", \"sender\": \"s\", \"message\": \"m\"}",
+                new CommandData("c", "s", "m", null));
+        testCommands.put("{\"commandName\": \"c\", \"sender\": \"s\", \"message\": \"m\",\"receiver\": \"r\" }",
+                new CommandData("c", "s", "m", "r"));
+    }
+
     @Test
     public void testParseCommand() throws Exception {
-        //todo
+        testCommands.forEach((json, cd) -> assertEquals(cd, commandManager.parseCommand(json)));
     }
 
     @Test
     public void testValidate() throws Exception {
-        //todo
+        assertTrue(false);
     }
 
     @Test
     public void testGetCommandAction() throws Exception {
-        //todo
+        assertTrue(false);
     }
 
 }

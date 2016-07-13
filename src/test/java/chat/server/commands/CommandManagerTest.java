b0de2efdbffe54257e34ce1c97d30146a2bb1b5c
Aleksey Dolgiy <AVDolgiy.SBT@sberbank.ru>
2016-07-13 12:50:59 +0300

    tests

--- src/test/java/chat/server/commands/CommandManagerTest.java
+++ src/test/java/chat/server/commands/CommandManagerTest.java
@@ -2,12 +2,16 @@ package chat.server.commands;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = "classpath:server-context.xml")
 public class CommandManagerTest {
+
+    @Autowired
+    private CommandManager commandManager;
 
     @Test
     public void testParseCommand() throws Exception {
